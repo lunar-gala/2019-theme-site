6f45bf4358015a6a23b692d2e717e3d7025a40ce
emmaloool <emmaliu@andrew.cmu.edu>
2019-01-14 18:11:30 -0500

    Added backpage (star) functionality

--- script/ticket.js
+++
@@ -1,7 +0,0 @@
-function dispLink() {
-    document.getElementById("ticket-window").innerHTML = "<h1>hello hello</h1>";
-}
-
-function hideLink() {
-    document.getElementById("ticket-window").innerHTML = "";
-}
