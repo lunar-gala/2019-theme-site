dfde5b99dd4d2ebf5b51c7c8ac2c373c1a23753e
swlsheltie <swl@andrew.cmu.edu>
2019-01-14 02:22:46 -0500

    arrows

--- script/index-interact.js
+++ script/index-interact.js
@@ -96,7 +96,8 @@ class Jello {
     // An array of images for background (.jpg)
     // They'll transition in the order listed below
     this.bgArray.push(
-      "images/backdrops/amallal.jpg",
+      "https://i.imgur.com/BDa5djf.jpg",
+      // "https://i.imgur.com/hb8v1Qd.jpg",
       "https://i.imgur.com/Z8LQt3Z.jpg",
       "https://i.imgur.com/UQkB2Lo.jpg",
       "https://i.imgur.com/3XyuoTy.jpg",
