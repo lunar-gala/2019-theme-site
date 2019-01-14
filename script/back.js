8be6fbb308538d8c57982d24bc25a29b6f4f42d2
emmaloool <33307632+emmaloool@users.noreply.github.com>
2019-01-14 18:19:16 -0500

    Update back.js

--- script/back.js
+++ script/back.js
@@ -1,9 +1,9 @@
 function dispBack() {
-    document.getElementById("star").style = "transform: rotateY(180deg)";
+    document.getElementById("star").innerHTML = "<img id='star' src='../images/archFrames/dark-star.svg' style='transform: rotateY(180deg);'>";
 }
 
 function hideBack() {
-    document.getElementById("nav-window").style = "none";
+    document.getElementById("nav-window").style = "<img id='star' src='images/archFrames/dark-star.svg'>";
 }
 
 function goBack() {
