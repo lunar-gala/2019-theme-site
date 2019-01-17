2deafbc361194f1b0e9be2d908d4131e1d22712f
emmaloool <emmaliu@andrew.cmu.edu>
2019-01-16 20:42:50 -0500

    Ran timer

--- script/timer.js
+++ script/timer.js
@@ -10,15 +10,15 @@ var x = setInterval(
                 //Find the distance between current date/time and the showtime.
                 var distance = showtime - now;
 
-                // Time calculations for days, hours, minutes and seconds
+                //Time calculations for days, hours, minutes and seconds
                 var d = Math.floor(distance / (1000 * 60 * 60 * 24));
                 var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                 var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
                 //Display day/hour/minutes in respective divs. 
-                document.getElementById("days").innerHTML = "<h1>" + d + "</h1>";
-                document.getElementById("hours").innerHTML = "<h1>" + h + "</h1>";
-                document.getElementById("minutes").innerHTML = "<h1>" + m + "</h1>";
+                document.getElementById("days").innerHTML = "<h2>" + d + "</h2>";
+                document.getElementById("hours").innerHTML = "<h2>" + h + "</h2>";
+                document.getElementById("minutes").innerHTML = "<h2>" + m + "</h2>";
              
                 if (distance < 0) {
                     //Clear the interval.
@@ -33,4 +33,4 @@ var x = setInterval(
                     document.getElementById("minutes").style.opacity = "0.0";
                 }
             }, 
-60 * 1000);
+1000);
