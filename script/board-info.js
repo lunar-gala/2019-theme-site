8841b7cbe55c1d198a8494355da7dab77e4deaf5
emmaloool <emmaliu@andrew.cmu.edu>
2019-01-23 17:54:55 -0500

    Set-up lines/board json

---
+++ script/board-info.js
@@ -0,0 +1,24 @@
+var execBoard = 
+    [
+        /* TEMPLATE: 
+        {
+            "firstname": "____", 
+            "lastname": "____",
+            "group": "____",
+            "position": "____",
+            "major": "____",
+            "year": ____,
+            "pictured": "____"
+        }
+        */
+
+        {
+            "firstname": "____", 
+            "lastname": "____",
+            "group": "____",
+            "position": "____",
+            "major": "____",
+            "year": ____,
+            "pictured": "____"
+        }
+    ]
