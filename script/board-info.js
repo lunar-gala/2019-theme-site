ba494fc2145ab05814f4495a61ed4bfff3cb3473
emmaloool <emmaliu@andrew.cmu.edu>
2019-01-26 14:37:17 -0500

    Updated personnel information for missing information

--- script/board-info.js
+++ script/board-info.js
@@ -2,23 +2,65 @@ var execBoard =
     [
         /* TEMPLATE: 
         {
-            "firstname": "____", 
-            "lastname": "____",
-            "group": "____",
+            "group": "BOARD",
+            "first": "____", 
+            "last": "____",
+            "team": "____",
             "position": "____",
+            "major": "____",
+            "year": "____",
+            "pictured": "____"
+        }
+
+        {
+            "group": "DESIGNER",
+            "first": "____", 
+            "last": "____",
             "major": "____",
             "year": ____,
             "pictured": "____"
         }
+
+        {
+            "group": "MODEL",
+            "first": "____", 
+            "last": "____",
+            "major": "____",
+            "year": ____,
+            "pictured": "____"
+        }
+
+        {
+            "group": "DANCER",
+            "first": "____", 
+            "last": "____",
+            "major": "____",
+            "year": ____,
+            "pictured": "____"
+        }
+
         */
 
         {
-            "firstname": "____", 
-            "lastname": "____",
-            "group": "____",
-            "position": "____",
-            "major": "____",
-            "year": ____,
+            "group": "BOARD",
+            "first": "Jumana", 
+            "last": "Akoad",
+            "team": "____",
+            "position": "Producer",
+            "major": "Psychology",
+            "year": "Junior",
+            "pictured": "Y"
+        }, 
+
+        {
+            "group": "BOARD",
+            "first": "Juan", 
+            "last": "Aranda",
+            "team": "Creative (Print)",
+            "position": "Print Designer",
+            "major": "Design",
+            "year": "Senior",
             "pictured": "____"
         }
+
     ]
