1b1c2652c12090fd258e38ec5f7a769416aaa4ff
swlsheltie <swl@andrew.cmu.edu>
2019-01-31 20:06:04 -0500

    click function

--- script/scrollnav.js
+++ script/scrollnav.js
@@ -1,5 +1,7 @@
-
-
+var info=[]
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+}
 
 window.onload = function() {
 
@@ -22,16 +24,58 @@ window.onload = function() {
   });
 
 
+
+}
+
+
   $(".people-list li").click(function(){
-    $(".people-list li").removeClass("active");
-    $(this).addClass("active");
-     });
+    
+
+    var this_li= $(this)
+    var name = this_li[0].innerHTML
+
+    //close name
+    if ($(this).hasClass("active")){
+      info=[]
+      console.log("close")
+      $(this).children().remove()
+      $(this).removeClass("active");
+    }
+
+    //open name
+    else {  
+      info=[]
+      console.log("open")
+
+      $(".people-list li").removeClass("active");
+      $(this).addClass("active");
+      //bring up text info
+      // console.log(this_li, name)
+      for (person in execBoard) {
+
+        if (getKeyByValue(execBoard[person], name)) {
+          console.log("in exec")
+          info.push(execBoard[person].position)
+          info.push(execBoard[person].major)
+          info.push(execBoard[person].year)
+        }
+      }
+      var node = document.createElement("SPAN");
+      var br = document.createElement("BR");
+      var textnode = document.createTextNode(info[0]+" "+info[1]);
+      var textnode2 = document.createTextNode(info[2]);
+      node.appendChild(textnode);
+      node.appendChild(br);
+      node.appendChild(textnode2);
+
+      $(this).children().remove()
+      $(this).append(node)
+     
+    }
+    
+  })
 
   $("#lil-ul li").click(function(){
     $("#lil-ul li").removeClass("switch");
     $(this).addClass("switch");
     });
-
-}
-
-
