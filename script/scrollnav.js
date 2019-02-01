c3a60c4d67d5114f04ec23d5d5f57189fe2eb6c9
swlsheltie <swl@andrew.cmu.edu>
2019-02-01 18:01:46 -0500

    Update people.html

--- script/scrollnav.js
+++ script/scrollnav.js
@@ -1,5 +1,4 @@
-
-
+var info=[]
 
 window.onload = function() {
 
@@ -22,17 +21,68 @@ window.onload = function() {
   });
 
 
+
+}
+
+
   $(".people-list li").click(function(){
-    $(".people-list li").removeClass("active");
-    $(this).addClass("active");
-     });
+   
+    var this_li= $(this)
+    var name = this_li[0].innerHTML
+
+    //close name
+    if ($(this).hasClass("active")){
+      info=[]
+      console.log("close")
+      $(this).children().remove()
+      $(this).removeClass("active");
+      $("#picture").attr("src","");
+      $("#side-pic").attr("src","");
+    }
+
+    //open name
+    else {  
+      info=[]
+      console.log("open")
+
+      $(".people-list li").removeClass("active");
+      $(".people-list li").children().remove();
+      $(this).addClass("active");
+      //bring up text info
+      // console.log(this_li, name)
+      for (person in execBoard) {
+        console.log(person, execBoard[person], name)
+        if (execBoard[person].name === name) {
+          console.log("in exec")
+          info.push(execBoard[person].position)
+          info.push(execBoard[person].major)
+          info.push(execBoard[person].year)
+          console.log(info)
+
+        }
+      }
+      var node = document.createElement("h6");
+      var br = document.createElement("BR");
+      var textnode = document.createTextNode(info[0]);
+      var textnode2 = document.createTextNode(info[1]+" "+info[2]);
+      node.appendChild(textnode);
+      node.appendChild(br);
+      node.appendChild(textnode2);
+
+      $(this).children().remove()
+      $(this).append(node) 
+
+      window.getComputedStyle(node).opacity;
+      console.log($(this).css("height"));
+      node.className += 'in';
+
+     $("#picture").attr("src","images/Headshots/"+name+".jpg");
+     $("#side-pic").attr("src","images/Headshots/"+name+"-side.jpg");
+    }
+    console.log($(this).css("height"));
+  })
 
   $("#lil-ul li").click(function(){
     $("#lil-ul li").removeClass("switch");
     $(this).addClass("switch");
     });
-
-
- 
-
-}
