b559013bed88081086a50a1398e874c000617189
emmaloool <emmaliu@andrew.cmu.edu>
2019-02-06 19:57:51 -0500

    FINISHED PPL JSON

--- script/peopleInfo.js
+++ script/peopleInfo.js
@@ -1,4 +1,4 @@
-var boardInfo = 
+var people = 
     [
         {
             "group": "BOARD",
@@ -349,12 +349,732 @@ var boardInfo =
             "position": "Head Modeling Coordinator",
             "major": "Biological Sciences",
             "year": "2020",
+        }, 
+
+        {
+            "group": "DESIGNER",
+            "name": "Mariana Alberola Rezza",
+            "line": "",
+            "major": "Architecture",
+            "year": "Masters",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Katie Boyle",
+            "line": "",
+            "major": "Economics",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Ashley Burbano",
+            "line": "",
+            "major": "Design",
+            "year": "2022",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Lily Cunicelli",
+            "line": "",
+            "major": "Drama",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Charlotte Deiss",
+            "line": "",
+            "major": "Electrical & Computer Engineering",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Daniela Delgado",
+            "line": "",
+            "major": "Design",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Vincent DeRienzo",
+            "line": "",
+            "major": "Architecture",
+            "year": "2020",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Alice Fang",
+            "line": "",
+            "major": "Design",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Yang Gao",
+            "line": "",
+            "major": "Architecture",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Zain Islam-Hashmi",
+            "line": "",
+            "major": "Architecture",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Min Young Jeong",
+            "line": "",
+            "major": "Architecture",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Mimi Jiao",
+            "line": "",
+            "major": "Design",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Rishi Karthikeyan",
+            "line": "",
+            "major": "Business Administration",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Leah Kendrick",
+            "line": "",
+            "major": "Architecture",
+            "year": "2020",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Rebecca Kim",
+            "line": "",
+            "major": "General (Dietrich)",
+            "year": "2022",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Yoon Young Kim",
+            "line": "",
+            "major": "Design, Human-Computer Interaction",
+            "year": "2020",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Sophia Kim",
+            "line": "",
+            "major": "Design",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "HaeWann (Christine) Kim",
+            "line": "",
+            "major": "Architecture",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Chaeyun (Sophie) Lee",
+            "line": "",
+            "major": "Architecture",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Nicole Lee-Park",
+            "line": "",
+            "major": "Modern Languages, Architecture",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Xiaoying Meng",
+            "line": "",
+            "major": "Architecture",
+            "year": "2020",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Tyng Peck",
+            "line": "",
+            "major": "Architecture",
+            "year": "2020",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "David Perry",
+            "line": "",
+            "major": "General (MCS)",
+            "year": "2020",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Laura Riviere",
+            "line": "",
+            "major": "General (Dietrich)",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Jaclyn Saik",
+            "line": "",
+            "major": "Design",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Somin Shim",
+            "line": "",
+            "major": "Architecture",
+            "year": "2019",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Stephanie Smid",
+            "line": "",
+            "major": "Architecture",
+            "year": "Masters",
+        },
+        
+        {
+            "group": "DESIGNER",
+            "name": "DK Wang",
+            "line": "",
+            "major": "Architecture",
+            "year": "2019",
+        },
+        
+        {
+            "group": "DESIGNER",
+            "name": "Veronica Wang",
+            "line": "",
+            "major": "Architecture",
+            "year": "2019",
+        },
+        
+        {
+            "group": "DESIGNER",
+            "name": "Jessa Westheimer",
+            "line": "",
+            "major": "Science and Arts",
+            "year": "2021",
+        },
+        
+        {
+            "group": "DESIGNER",
+            "name": "Arden Wolf",
+            "line": "",
+            "major": "Art",
+            "year": "2021",
+        },
+       
+        {
+            "group": "DESIGNER",
+            "name": "Nina Yoo",
+            "line": "",
+            "major": "Design",
+            "year": "2021",
+        },
+        
+        {
+            "group": "DESIGNER",
+            "name": "Clara Zhao",
+            "line": "",
+            "major": "Architecture",
+            "year": "2021",
+        },
+
+        {
+            "group": "DESIGNER",
+            "name": "Ellen Zhu",
+            "line": "",
+            "major": "Architecture",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Xavier Apostol",
+            "major": "Art",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Graham Arthur",
+            "major": "Statistics and Data Science",
+            "year": "Masters",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Marvin Bennett Jr.",
+            "major": "Mechanical Engineering",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Christopher Bright",
+            "major": "Mechanical Engineering",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Alex Brooks",
+            "major": "General (Dietrich)",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Monica Chang",
+            "major": "Interdisciplinary (Dietrich), Human-Computer Interaction",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Tina Chen",
+            "major": "Business Administration, Human-Computer Interaction",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Helen Cheng",
+            "major": "Business Adminsitration",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Junwoo Cheong",
+            "major": "Design",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Julie Choi",
+            "major": "Design",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Hannah Cornish",
+            "major": "General (Dietrich)",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Baris Denizli",
+            "major": "General (Dietrich)",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Mike Dong",
+            "major": "Chemistry",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Fiona Dubrosa",
+            "major": "Social & Decision Sciences",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Leah Ettensohn",
+            "major": "Psychology",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Olivia Fernau",
+            "major": "General (CIT)",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Ashwini Ganpule",
+            "major": "Mechanical Engineering, Engineering & Public Policy",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Patrick Gao",
+            "major": "Electrical & Computer Engineering",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Danova Gardilcic",
+            "major": "Art",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Franklin Guttman",
+            "major": "Design",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Korey Henderson",
+            "major": "Biological Sciences",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Universal Hunte",
+            "major": "Art",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Sung Ho Hwang",
+            "major": "Statistics and Data Science",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Phil Johnson",
+            "major": "General (Dietrich)",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Simone Jones",
+            "major": "Drama",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Zoeanne Juang",
+            "major": "Stastics and Data Science",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Esther Kim",
+            "major": "Biological Sciences",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Irina Kramkova",
+            "major": "General (Dietrich)",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Angela Lee",
+            "major": "Design",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Julia Lee",
+            "major": "Computer Science",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Katherine Li",
+            "major": "Mathematical Sciences, Institute for Politics and Strategy",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Connor Maggio",
+            "major": "Electrical & Computer Engineering",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Terrence Mims",
+            "major": "____",
+            "year": "___",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Catherine Mondoa",
+            "major": "Materials Science & Engineering",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Ruhani Mumick",
+            "major": "Biological Sciences",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Lera Neviadomska",
+            "major": "Interdisciplinary (Dietrich)",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Gloria Oh",
+            "major": "Biological Sciences",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Sammie Paul",
+            "major": "Drama",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Jonathan Perez",
+            "major": "Creative Writing, Fine Art, Computer Science",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Martin Pillich",
+            "major": "Chemical Engineering",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Michael Powell",
+            "major": "Architecture",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Pauline Qin",
+            "major": "Music",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Zee Salman",
+            "major": "Design",
+            "year": "2022",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Julianne Sanscartier",
+            "major": "Mechanical Engineering",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Christina Sellountos",
+            "major": "Art",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Sachi Shah",
+            "major": "Art",
+            "year": "Sophomore",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Francesca Starvaggi",
+            "major": "Chemistry",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Alexandra Van Praag",
+            "major": "Mathematical Sciences",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Mia Venezia",
+            "major": "Music",
+            "year": "2019",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Janice Wan",
+            "major": "Business Administration",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Olivia Werner",
+            "major": "Architecture",
+            "year": "2021",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Ugne Ziausyte",
+            "major": "Biological Sciences",
+            "year": "2020",
+        },
+
+        {
+            "group": "MODEL",
+            "name": "Mia Zurovac",
+            "major": "Drama",
+            "year": "2022",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Alec Albright",
+            "major": "Humanities and Arts",
+            "year": "2020",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Amelia Chan",
+            "major": "General (CIT)",
+            "year": "2022",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Amy Chen",
+            "major": "Psychology",
+            "year": "2019",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Sabrina Clarke",
+            "major": "Philosophy",
+            "year": "2019",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Albert Du",
+            "major": "____",
+            "year": "___",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Yu Xi Lee",
+            "major": "General (SCS)",
+            "year": "2022",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Miranda Lin",
+            "major": "Computer Science",
+            "year": "2021",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Julia Luo",
+            "major": "Interdisciplinary (Dietrich), Human-Computer Interaction",
+            "year": "2019",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Beata Purvina",
+            "major": "Business Administration",
+            "year": "2020",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Victoria Vanbenschoten",
+            "major": "Science and Humanities Scholars",
+            "year": "2019",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Sarah Wang",
+            "major": "Biological Sciences",
+            "year": "2020",
+        },
+
+        {
+            "group": "DANCER",
+            "name": "Chaya Wurman",
+            "major": "Computational Biology",
+            "year": "2019",
         }
     ];
-
-    /*  {
-            "group": "___",
-            "name": "___",
-            "major": "____",
-            "year": "____",
-        } */
